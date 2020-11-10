b0affa19e0c8223e9ec880c12544e2be7d1760bc
anton <anton@DESKTOP-NF7MRDF>
2020-11-10 20:04:09 +0200

    Read file car-f-92.stu and console output

---
+++ src/Algorithms_and_Complexity/algo.java
@@ -0,0 +1,41 @@
+package Algorithms_and_Complexity;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class algo {
+
+	public static void main(String[] args) {
+		String line="";
+		String csvSplitBy=" ";
+		ArrayList<ArrayList> car_f_92=new ArrayList<>();
+		
+		int count=0;
+		try(BufferedReader br=new BufferedReader(new FileReader("datasets/car-f-92.stu"))){
+			while ((line=br.readLine())!=null) {
+				String[] data = line.split(csvSplitBy);
+				ArrayList<String> file_integers=new ArrayList<>();
+				for(int i=0;i<data.length;i++){
+					file_integers.add(data[i]);
+				}
+				car_f_92.add(file_integers);
+				count++;
+				System.out.println(count+" Φοιτητής και είναι εγεγγραμένος σε αυτά τα μαθήματα: " +file_integers);
+				if(count==18419)
+				{
+					if(file_integers.get(0).equals(file_integers.get(1))){
+					System.out.println("Thee mou");
+					System.out.println(file_integers.get(0));
+					System.out.println(file_integers.get(1));
+					}
+				}
+			}
+		}catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
